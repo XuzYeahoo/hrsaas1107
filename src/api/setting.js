b1284b9f012143e25e590e72a8ddd5a308e81eae
XuzYeahoo <1285328566@qq.com>
2022-11-13 13:44:27 +0800

    读取公司角色信息

---
+++ src/api/setting.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+// 获取角色列表
+export const getRoleList = (params) => {
+  return request.get('/sys/role', {
+    params
+  })
+}
+
+// 获取公司信息
+export const getCompanyInfo = (companyId) => {
+  return request.get(`/company/${companyId}`)
+}
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -5,23 +5,39 @@
         <el-tabs>
           <el-tab-pane label="角色管理">
             <!-- 新增角色按钮 -->
-            <el-row style="height:60px">
-              <el-button type="primary" size="small" icon="el-icon-plus">新增角色</el-button>
+            <el-row style="height: 60px">
+              <el-button
+                type="primary"
+                size="small"
+                icon="el-icon-plus"
+              >新增角色</el-button>
             </el-row>
             <!-- 表格 -->
-            <el-table border>
-              <el-table-column label="序号" width="120" />
-              <el-table-column label="角色名称" width="240" />
-              <el-table-column label="描述" />
-              <el-table-column label="操作">
+            <!-- 给表格绑定数据 -->
+            <el-table border :data="list">
+              <el-table-column align="center" type="index" label="序号" width="120" />
+              <el-table-column align="center" prop="name" label="角色名称" width="240" />
+              <el-table-column align="center" prop="description" label="描述" />
+              <el-table-column align="center" label="操作">
                 <el-button size="small" type="success">分配权限</el-button>
                 <el-button size="small" type="primary">编辑</el-button>
                 <el-button size="small" type="danger">删除</el-button>
               </el-table-column>
             </el-table>
             <!-- 分页组件 -->
-            <el-row type="flex" justify="center" align="middle" style="height: 60px">
-              <el-pagination layout="prev, pager, next" />
+            <el-row
+              type="flex"
+              justify="center"
+              align="middle"
+              style="height: 60px"
+            >
+              <el-pagination
+                :current-page="page.page"
+                :page-size="page.pagesize"
+                :total="page.total"
+                layout="prev, pager, next"
+                @current-change="changePage"
+              />
             </el-row>
           </el-tab-pane>
           <el-tab-pane label="公司信息">
@@ -32,21 +48,27 @@
               :closable="false"
               :show-icon="true"
             />
-            <el-form label-width="120px" style="margin-top:50px">
+            <el-form label-width="120px" style="margin-top: 50px">
               <el-form-item label="企业名称">
-                <el-input disabled style="width:400px" />
+                <el-input v-model="formData.name" disabled style="width: 400px" />
               </el-form-item>
               <el-form-item label="公司地址">
-                <el-input disabled style="width:400px" />
+                <el-input v-model="formData.companyAddress" disabled style="width: 400px" />
               </el-form-item>
               <el-form-item label="电话">
-                <el-input disabled style="width:400px" />
+                <el-input v-model="formData.companyPhone" disabled style="width: 400px" />
               </el-form-item>
               <el-form-item label="邮箱">
-                <el-input disabled style="width:400px" />
+                <el-input v-model="formData.mailbox" disabled style="width: 400px" />
               </el-form-item>
               <el-form-item label="备注">
-                <el-input type="textarea" :rows="3" disabled style="width:400px" />
+                <el-input
+                  v-model="formData.remarks"
+                  type="textarea"
+                  :rows="3"
+                  disabled
+                  style="width: 400px"
+                />
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -57,11 +79,45 @@
 </template>
 
 <script>
+import { getRoleList, getCompanyInfo } from '@/api/setting'
+import { mapGetters } from 'vuex'
 export default {
-
+  data() {
+    return {
+      list: [], // 承接数组
+      page: {
+        // 放置页码及相关数据
+        page: 1,
+        pagesize: 2,
+        total: 0 // 记录总数
+      },
+      formData: {}
+    }
+  },
+  computed: {
+    ...mapGetters(['companyId'])
+  },
+  created() {
+    this.getRoleList()
+    this.getCompanyInfo()
+  },
+  methods: {
+    async getRoleList() {
+      const { total, rows } = await getRoleList(this.page)
+      this.page.total = total
+      this.list = rows
+    },
+    changePage(newPage) {
+      // newPage是当前点击的页码
+      this.page.page = newPage
+      // 重新渲染页面
+      this.getRoleList()
+    },
+    async getCompanyInfo() {
+      this.formData = await getCompanyInfo(this.companyId)
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
